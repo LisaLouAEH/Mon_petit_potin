81a262bc64deffdcf341eea0dd5bd36f119dd47b
Lisa Lou <lisa.lou.aeh@gmail.com>
2018-10-30 15:19:17 +0100

    finish

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get '/', to: 'potin#index' #path
   get '/team', to:'static#team'
-  get '/Accueil', to:"static#acceuil"
-  get '/contact', to:"static#contact"
+  get '/Accueil', to:'static#acceuil'
+  get '/contact', to:'static#contact'
   get '/show/:id', to: 'potin#show'
+  get '/new', to:'potin#new'
   #get 'potins/:id', to: 'potins#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
